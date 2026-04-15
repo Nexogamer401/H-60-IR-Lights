171241ae4a176216de84a9430864822ea67fc99e
Nexogamer401 <65250809+Nexogamer401@users.noreply.github.com>
2026-04-15 22:35:10 +0200

    Update cfgHctCockpit.hpp

--- addons/UH60/config/cfgVehiclesParts/cfgHctCockpit.hpp
+++ addons/UH60/config/cfgVehiclesParts/cfgHctCockpit.hpp
@@ -71,13 +71,13 @@ class interaction {
         class Lights_mode {
             positionType="static";
             position="b_lights_mode";
-            label="External Light Mode";
+            label="Ext Light Mode";
             radius=BTN_RADIUS_SWITCH;
             animation="Switch_lights_cockpit";
             animSpeed=0;
             animStates[] = {0,0.5,1};
             animLabels[] = {"NORM","SEARCH LT", "ALL IR"};
-            animEnd="_this call vtx_uh60_misc_fnc_toggleLightMode";
+            animEnd="[_this # 0, ""SEARCH_LT"", ""ALL_IR""] call vtx_uh60_misc_fnc_toggleLightMode";
             clickSound="hct_Switch_Sound";
         }; // Lights_position
         class fuelPump {
